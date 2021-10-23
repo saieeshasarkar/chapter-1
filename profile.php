da068e160a65536a61268fd2e878aaa6bd838ce6
saieeshasarkar <90947912+saieeshasarkar@users.noreply.github.com>
2021-10-23 14:31:51 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,11 @@
 <HTML>
   <title> profile </title>
   <body>
+    <ol>
     <li> Saieesha Sarkar </li>
     <li> 12 years old </li>
-    <li> 020 5555 555 0 </li> </ol> 
+    <li> 020 5555 555 0 </li>
+    </ol> 
     <br><a href=https://phpchapter.herokuapp.com/> link </a>
     
     
